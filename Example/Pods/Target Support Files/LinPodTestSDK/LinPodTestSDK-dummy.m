5283a12304470b0d2ee1fbc0a4f514bd4367ccf7
chw <1794437065@qq.com>
2021-04-22 16:12:04 +0800

    Initial Commit

---
+++ Example/Pods/Target Support Files/LinPodTestSDK/LinPodTestSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LinPodTestSDK : NSObject
+@end
+@implementation PodsDummy_LinPodTestSDK
+@end
---
+++ Example/Pods/Target Support Files/Pods-LinPodTestSDK_Tests/Pods-LinPodTestSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LinPodTestSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LinPodTestSDK_Tests
+@end
